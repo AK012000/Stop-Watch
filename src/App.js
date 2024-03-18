dedec840547be1bf64ad130422062a351f5211f8
AK012000 <arkaushal2000@gmail.com>
2024-03-19 00:00:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+var adjustInterval = undefined;
 
 function App() {
+  const [watch,setWatch] = useState(0)
+  const [started,setstarted] = useState(false)
+ 
+ 
+  const startWatch = ()=>{
+   adjustInterval = setInterval(()=> {
+   setWatch((x)=>x+1)
+   },1000)
+   setstarted(true)
+  }
+
+   const stopWatch = ()=> {
+  clearInterval(adjustInterval) 
+  setstarted(false)
+  }
+ 
+   const resetWatch = ()=> {
+   setWatch(0)
+   clearInterval(adjustInterval)
+   setstarted(false)
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Stop Watch</h1>
+    <h1>{watch}</h1>
+    <button className='start' disabled={started} onClick={startWatch}>Start</button>
+    <button className='stop' onClick={stopWatch}>Stop</button>
+    <button className='reset' onClick={resetWatch}>Reset</button>
+
+        </div>
   );
 }
+
 
 export default App;
